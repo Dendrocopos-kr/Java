24db7fd1e5438b648aebc981530c87cdf7a1ce10
Dendrocopos-kr <Dendrocopos-kr@users.noreply.github.com>
2020-06-14 01:15:42 +0900

    Java

---
+++ first/src/kr/co/dendrocopos/first/Variable3.java
@@ -0,0 +1,14 @@
+package kr.co.dendrocopos.first;
+
+public class Variable3 {
+	public static void main(String[] args) {
+		int n1 = 2147483647;
+		long n4 = 222222;
+		float n2 = 50.2f;
+		double n3 = n2;
+		
+		
+		System.out.println(n1);
+		System.out.println(n2);
+	}
+}
