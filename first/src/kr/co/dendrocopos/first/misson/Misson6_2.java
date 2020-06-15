50cb4b460e05371b0be6408f16c97e9a9a22f97b
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-06-15 14:22:11 +0900

    t1

--- first/src/kr/co/dendrocopos/first/misson/Misson6_2.java
+++ first/src/kr/co/dendrocopos/first/misson/Misson6_2.java
@@ -2,7 +2,7 @@ package kr.co.dendrocopos.first.misson;
 
 public class Misson6_2 {
 	public static void main(String[] args) {
-		int star = 10;
+		int star = 15;
 		for ( int i = 0 ; i < star ; i++) {
 			System.out.print("*");
 		}
