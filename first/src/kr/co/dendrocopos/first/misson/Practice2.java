88ac86dfcd479e44c5152e1fa116903723cb4c54
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-06-15 17:51:40 +0900

    practice2_1

--- first/src/kr/co/dendrocopos/first/misson/Practice2.java
+++ first/src/kr/co/dendrocopos/first/misson/Practice2.java
@@ -7,8 +7,19 @@ public class Practice2 {
 		//랜덤값 뽑기
 		
 		Random random_num = new Random();
-		int a  = random_num.nextInt(100) - 50;
+		int ten  = random_num.nextInt(3) + 1;
+		int one = random_num.nextInt(10) + 1;
+				
+		int[][] card = new int[4][13];
 		
-		System.out.println(a);
+		for(int i = 0 ; i < 4 ; i ++)
+		{
+			for( int j = 0; j < 13; j++)
+			{
+				card[i][j] = j > 10 ? 11 : j+1 ;
+				//System.out.printf( "%d ", card[i][j]);
+			}
+			//System.out.println();
+		}
 	}
 }
