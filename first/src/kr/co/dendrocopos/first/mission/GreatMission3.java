4f7c90c8792233be3086dc18e0ee5f6f904ff5a4
Dendrocopos-kr <Dendrocopos-kr@users.noreply.github.com>
2020-06-17 19:02:35 +0900

    과제 끝

--- first/src/kr/co/dendrocopos/first/mission/GreatMission3.java
+++ first/src/kr/co/dendrocopos/first/mission/GreatMission3.java
@@ -2,39 +2,40 @@ package kr.co.dendrocopos.first.mission;
 
 public class GreatMission3 {
 	public static void main(String[] args) {
-		int[][] arr = {
+
+		String[] strSubject = { "영어","수학","국어","사회"};
+
+		int[] Sum = new int[strSubject.length];
+		float[] Avg = new float[strSubject.length];
+		int tSum = 0;
+		float tAvg = 0.0f;
+		
+		int[][] arrSubject = {
 				{ 90, 100, 88 }, //영어
 				{ 34, 99, 45},	//수학
-				{ 98, 65, 87}	//국어
+				{ 98, 65, 87},	//국어
+				{ 49, 51, 63} //사회
 		};
 		
-		String[] subject = { "영어","수학","국어"};
-		
-		int[] Sum = new int[subject.length];
-		float[] Avg = new float[subject.length];
-		
-		for(int i = 0 ; i < arr.length ; i++){
-			for(int j = 0 ; j < arr[i].length; j++) {
-				Sum[i] += arr[j][i];
+		for(int i = 0 ; i < strSubject.length ; i++){
+			for(int j = 0 ; j < arrSubject[i].length; j++) {
+				Sum[i] += arrSubject[i][j];
 			}
 		}
-		for(int i = 0 ; i < arr.length ; i++){
-			Avg[i] = Sum[i] / arr[i].length;
+		
+		for(int i = 0 ; i < strSubject.length ; i++){
+			Avg[i] = Sum[i] / arrSubject[i].length;
+		}		
+		
+		for( int i = 0 ; i < strSubject.length; i++) {
+			tSum += Sum[i];
+			tAvg += Avg[i];
 		}
 		
-		
-		for( int i = 0 ; i < subject.length; i++) {
-			System.out.println(subject[i] + ": " + Sum[i] + ", " + "평균");
+		for( int i = 0 ; i < strSubject.length; i++) {
+			System.out.println(strSubject[i] + "점수: " + Sum[i] + ", " + "평균점수: " + Avg[i]);
 		}
-		System.out.printf(
-				  "영어: %d, 평균: %3.2f\n"
-				+ "수학: %d, 평균: %3.2f\n"
-				+ "국어: %d, 평균: %3.2f\n"
-				+ "전체: %d, 평균: %3.2f",
-				eSum,eAvg,
-				mSum,mAvg,
-				lSum,lAvg,
-				eSum+mSum+lSum,(eAvg+mAvg+lAvg/arr.length)
-				);
+		System.out.printf("전체점수: %d, 전체평균: %.1f",tSum, tAvg/strSubject.length);
+		
 	}
 }
