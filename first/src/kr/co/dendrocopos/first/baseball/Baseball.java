7b1eab8ab92ec373c84efdb8ebc164f9179970b1
Dendrocopos-kr <Dendrocopos-kr@users.noreply.github.com>
2020-06-22 20:40:18 +0900

    #baseball - get;set; 전환

--- first/src/kr/co/dendrocopos/first/baseball/Baseball.java
+++ first/src/kr/co/dendrocopos/first/baseball/Baseball.java
@@ -5,15 +5,34 @@ import java.util.Scanner;
 public class Baseball {
 	// DEFAULT (자료형) => private + package;
 	private int nBaseballs;
+	public int getnBaseballs() {return nBaseballs;}
+	public void setnBaseballs(int nBaseballs) {this.nBaseballs = nBaseballs;}
+	
 	private int nRange;
-
+	public int getnRange() {return nRange;}
+	public void setnRange(int nRange) {this.nRange = nRange;}
+	
 	private int nBaseballsCheck;
+	public int getnBaseballsCheck() {return nBaseballsCheck;}
+	public void setnBaseballsCheck(int nBaseballsCheck) {this.nBaseballsCheck = nBaseballsCheck;}
+	
 	private int nRangeCheck;
+	public int getnRangeCheck() {return nRangeCheck;}
+	public void setnRangeCheck(int nRangeCheck) {this.nRangeCheck = nRangeCheck;}
+	
+	private int[] arrBallNumber;
+	public int[] getArrBallNumber() {return arrBallNumber;}
+	public int getBallNumber(int i) {return arrBallNumber[i];}
+	public void setBallNumber(int i, int arrBallNumber) {this.arrBallNumber[i] = arrBallNumber;}
+	public void setArrBallNumber(int[] arrBallNumber) {this.arrBallNumber = arrBallNumber;}
+	
+	private int[] arrThrowNumber;
+	public int[] getArrThrowNumber() {return arrThrowNumber;}
+	public int getThrowNumber(int i) {return arrThrowNumber[i];}
+	public void setThrowNumber(int i,int arrThrowNumber) {this.arrThrowNumber[i] = arrThrowNumber;}
+	public void setArrThrowNumber(int[] arrThrowNumber) {this.arrThrowNumber = arrThrowNumber;}
 
 	private int nTry;
-	private int[] arrBallNumber;
-	private int[] arrThrowNumber;
-
 	private Scanner scan;
 
 	// 생성자
@@ -22,8 +41,8 @@ public class Baseball {
 	}
 
 	private Baseball(int nBaseballs, int nRange) { // 입력받은 받은 공 개수와 범위 저장, scan 생성
-		this.nBaseballs = nBaseballs;
-		this.nRange = nRange;
+		setnBaseballs(nBaseballs);
+		setnRange(nRange);
 		scan = new Scanner(System.in);
 	}
 
@@ -37,32 +56,35 @@ public class Baseball {
 	private void InsertBaseballsNRange() { // 공개수와 범위 입력
 		System.out.println("숫자 야구게임을 시작합니다. \n\n맞출 숫자의 갯수와 범위를 정해주세요");
 		System.out.print("맞출숫자 < 맞출범위 :");
-		nBaseballsCheck = scan.nextInt();
-		nRangeCheck = scan.nextInt();
+		setnBaseballsCheck(scan.nextInt());
+		setnRangeCheck(scan.nextInt());
 	}
 
 	private void CheckingSet() { // 범위와 공 개수 확인 ( 공 0개이하 or 공개수 >= 범위면 초기값 공3,범위1~9 )
-		if (nBaseballsCheck < 1 || nRangeCheck <= nBaseballsCheck) {
+		
+		if (getnBaseballsCheck() < 1 || getnRangeCheck() <= getnBaseballsCheck()) {
 			System.out.println("설정 오류로 기본셋팅으로 시작합니다.");
-			System.out.printf("맞출 숫자 %d개, 범위 1~%d 입니다.\n", nBaseballs, nRange);
+			System.out.printf("맞출 숫자 %d개, 범위 1~%d 입니다.\n", getnBaseballs(), getnRange());
 		} else {
-			System.out.printf("맞출 숫자 %d개, 범위 1~%d 입니다.\n", nBaseballsCheck, nRangeCheck);
-			nBaseballs = nBaseballsCheck;
-			nRange = nRangeCheck;
+			System.out.printf("맞출 숫자 %d개, 범위 1~%d 입니다.\n", getnBaseballsCheck(), getnRangeCheck());
+			setnBaseballs(getnBaseballsCheck());
+			setnRange(getnRangeCheck());
 		}
-		arrBallNumber = new int[nBaseballs];
-		arrThrowNumber = new int[nBaseballs];
+		arrBallNumber = new int[getnBaseballs()];
+		arrThrowNumber = new int[getnBaseballs()];
 	}
 
 	private void SuffleBallNumber() { // 숫자 섞기
 		// int minRandomNumber = 1; // 최소시작점 셋팅을 하지않음. 1부터 시작임.
-		int maxRandomNumber = nRange;
-		for (int i = 0; i < nBaseballs; i++) {
-			arrBallNumber[i] = (int) (Math.random() * maxRandomNumber) + 1;
+		int maxRandomNumber = getnRange();
+		for (int i = 0; i < getnBaseballs(); i++) {
+
+			setBallNumber(i,(int) (Math.random() * maxRandomNumber) + 1);
+			//arrBallNumber[i] = (int) (Math.random() * maxRandomNumber) + 1;
 			// arrBallNumber[i] = (int) (Math.random() * maxRandomNumber - minRandomNumber +
 			// 1) + minRandomNumber; // 최소 시작점 셋팅시 윗 라인과 변경
 			for (int j = 0; j < i; j++) {
-				if (arrBallNumber[i] == arrBallNumber[j]) {
+				if (getBallNumber(i) == getBallNumber(j)) {
 					i--;
 					break;
 				}
@@ -77,7 +99,7 @@ public class Baseball {
 	}
 
 	private void StartGame() { // 시작문구 출력
-		System.out.printf("------- %d 숫자 야구게임 ------- \n", nBaseballs);
+		System.out.printf("------- %d 숫자 야구게임 ------- \n", getnBaseballs());
 	}
 
 	public boolean CheckingSBO() { // 숫자 확인 SBO 출력
@@ -85,9 +107,9 @@ public class Baseball {
 		int nStrike = 0;
 		int nOut;
 		nTry++;
-		for (int i = 0; i < nBaseballs; i++) {
-			for (int j = 0; j < nBaseballs; j++) {
-				if(arrBallNumber[i] == arrThrowNumber[j]) {
+		for (int i = 0; i < getnBaseballs(); i++) {
+			for (int j = 0; j < getnBaseballs(); j++) {
+				if(getBallNumber(i) == getThrowNumber(j)) {
 					if(i == j)
 					{
 						nStrike++;
@@ -97,35 +119,45 @@ public class Baseball {
 				}
 			}
 		}
-		nOut = nBaseballs - nStrike - nBall;
+		nOut = getnBaseballs() - nStrike - nBall;
 		System.out.println("S: " + nStrike + "/ B:" + nBall + "/ O: " + nOut + "\n");
 
-		return (nStrike == nBaseballs) ? true : false;
+		return (nStrike == getnBaseballs()) ? true : false;
 	}
 
 	public void ThrowNumber() { // 숫자 입력 받기
-		System.out.printf("숫자 %d개를 입력하세요.", nBaseballs);
-		for (int i = 0; i < nBaseballs; i++) {
-			arrThrowNumber[i] = scan.nextInt();
+		System.out.printf("숫자 %d개를 입력하세요.", getnBaseballs());
+		for (int i = 0; i < getnBaseballs(); i++) {
+			setThrowNumber(i,scan.nextInt());
+			//arrThrowNumber[i] = scan.nextInt();
 			RangeCheck(i);
 		}
 	}
 
 	private void RangeCheck(int selectedNumber) { // 범위 밖의 숫자 입력시
 		while (true) {
+			if(getThrowNumber(selectedNumber) >= 1 && getThrowNumber(selectedNumber) <= getnRange()) {
+				break;
+			}else {
+				System.out.printf("입력값 : %d, 범위를 벗어났습니다. 범위( 1~%d )\n다시 입력해주세요. : ", getThrowNumber(selectedNumber), getnRange());
+				setThrowNumber(selectedNumber,scan.nextInt());
+			}
+			/*
 			if (arrThrowNumber[selectedNumber] >= 1 && arrThrowNumber[selectedNumber] <= nRange) {
 				break;
 			} else {
 				System.out.printf("입력값 : %d, 범위를 벗어났습니다. 범위( 1~%d )\n다시 입력해주세요. : ", arrThrowNumber[selectedNumber], nRange);
 				arrThrowNumber[selectedNumber] = scan.nextInt();
 			}
+			*/
 		}
 	}
 
 	public void Release() { // 정답출력, scan 닫기
 		System.out.printf("도전횟수 : %d \n정답 : ", nTry);
-		for (int i = 0; i < nBaseballs; i++) {
-			System.out.print(arrBallNumber[i] + " ");
+		for (int i = 0; i < getnBaseballs(); i++) {
+			System.out.print( getBallNumber(i) + " ");
+			//System.out.print(arrBallNumber[i] + " ");
 		}
 		scan.close();
 	}
