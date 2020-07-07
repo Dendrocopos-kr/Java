9497b4a283820b9331bba548995d97e31e74584f
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-07-07 12:22:13 +0900

    #블랙잭

--- first/src/kr/co/dendrocopos/second/blackjack/Dealer.java
+++ first/src/kr/co/dendrocopos/second/blackjack/Dealer.java
@@ -2,6 +2,7 @@ package kr.co.dendrocopos.second.blackjack;
 
 public class Dealer extends Gamer{
 	private final int DEALER_DRAWCUT = 17;
+	
 	@Override
 	public void moreCards(CardDeck cd) {
 		
--- first/src/kr/co/dendrocopos/second/blackjack/Gamer.java
+++ first/src/kr/co/dendrocopos/second/blackjack/Gamer.java
@@ -6,51 +6,73 @@ import java.util.Scanner;
 
 public class Gamer {
 	private List<Card> deck = new LinkedList<Card>();
-	
+	private Scanner sc = new Scanner(System.in);
+
 	public void receiveCard(Card c) {
 		deck.add(c);
+		//deck.add(AtoOne(c));
 	}
-	
+	/*
+	private Card AtoOne(Card c) {
+		if(c.getDenomination() == "A" && !(this instanceof Dealer)) {
+			if (getTotalPoint() <= 10) {
+				System.out.println("A를 11로 쓰시겠습니까?(y/n)");
+				String str = sc.next();
+				if ("N".equals(str) || "n".equals(str)) {
+					return c;
+				} else if ("Y".equals(str) || "y".equals(str)) {
+					return new Card(c.getPattern(), "A", 11);
+				}
+			}
+		}
+		return c;		
+	}
+	*/
+
 	protected int getTotalPoint() {
 		int sum = 0;
-		for(Card c: deck) {
+		for (Card c : deck) {
 			sum += c.getPoint();
 		}
 		return sum;
 	}
-	
+
 	private String getTotalCards() {
 		String str = "";
-		for(Card c: deck) {
-			str += c+"\n";
+		for (Card c : deck) {
+			str += c + "\n";
 		}
 		return str;
 	}
-	
+
 	public void openCards() {
-		String str = getTotalCards() + " 점수: " +getTotalPoint() ;
+		String str = getTotalCards() + " 점수: " + getTotalPoint();
 		System.out.println(str);
 	}
-	
+
 	@Override
 	public String toString() {
 		openCards();
 		return "";
 	}
-	
+
 	public void moreCards(CardDeck cd) {
-		Scanner scan = new Scanner(System.in);
+		// 현재 카드 출력
 		openCards();
 
-		while( getTotalPoint() < 21) {
+		// 더받을지 출력
+		while (getTotalPoint() < 21) {
+			
 			System.out.println("더 받으시겠습니까?(Y/N)");
-			String str = scan.next();
-			if(!("Y".equals(str) || "y".equals(str) )) {
+			String str = sc.next();
+			if ("N".equals(str) || "n".equals(str)) {
 				break;
-			}		
-			receiveCard(cd.getCard());
-			openCards();
+			} else if ("Y".equals(str) || "y".equals(str)) {
+				receiveCard(cd.getCard());
+				// 받고 나서 카드 확인용 출력
+				openCards();
+			}
 		}
-		scan.close();
+		sc.close();
 	}
 }
